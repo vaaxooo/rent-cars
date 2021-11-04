150aadb0dd7fe6f629cd5c2e98f2a19eb32db6e1
Nikita Aleksandrovich <Besedino_33>
2021-11-04 14:16:21 +0200

    -upldate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('role')->default('admin');
             $table->timestamp('email_verified_at')->nullable();
